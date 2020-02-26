93fdb618f5e19dd187eafb19198adfe5bb1d22ed
WalterJohnson0 <wang_ganyu@hotmail.com>
2020-02-25 21:36:14 -0500

    Added checkpoint to save model
    saved model in every check point.

--- DeepSpeech.py
+++ DeepSpeech.py
@@ -16,11 +16,14 @@ from util.Feeding import DataGenerator
 from util.Flags import create_flags, FLAGS
 import absl.app
 
+import os
+
 from Model1 import create_model, ctc_loss, optimizer
 
 
 def main(_):
     
+    # generator 
     data_preprocess_params = {'batch_size': FLAGS.batch_size,
                               'time_step': FLAGS.time_step_length, 
                               'window_len': FLAGS.window_length,
@@ -30,16 +33,35 @@ def main(_):
     train_generator = DataGenerator(FLAGS.train_file_path, FLAGS.alphabet_path, **data_preprocess_params)
     dev_generator = DataGenerator(FLAGS.dev_file_path, FLAGS.alphabet_path, **data_preprocess_params)
 
+    # check point
+    # Include the epoch in the file name (uses `str.format`)
+    checkpoint_path = "checkpoint1/model1-{epoch:04d}.ckpt"
+    checkpoint_dir = os.path.dirname(checkpoint_path)
+
+    # Create a callback that saves the model's weights every 5 epochs
+    cp_callback = keras.callbacks.ModelCheckpoint(
+                                                filepath=checkpoint_path, 
+                                                verbose=1, 
+                                                save_weights_only=True,
+                                                period=1)
     
+    # model
     model = create_model()
     
+    model.save_weights("checkpoint/cp0") #initial checkpoint
+    
     model.compile(optimizer=optimizer, loss=ctc_loss)
+    
     model.fit_generator(generator=train_generator,
                         validation_data=dev_generator,
-                        use_multiprocessing=False)      # True 
+                        use_multiprocessing=False,
+                        epochs=10,
+                        )
+    
     
     
 if __name__ == "__main__":
     create_flags()
     absl.app.run(main)
+
 
